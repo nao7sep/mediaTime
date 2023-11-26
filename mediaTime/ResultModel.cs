5d47c85d49569d9d6ff42c3d0fd7dbdef810e278
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2023-11-26 16:44:40 +0900

    ResultModel を追加

---
+++ mediaTime/ResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace mediaTime
+{
+    public class ResultModel
+    {
+        [JsonPropertyName ("file_path")]
+        public string? FilePath { get; set; }
+
+        [JsonPropertyName ("new_file_path")]
+        public string? NewFilePath { get; set; }
+
+        [JsonPropertyName ("type")]
+        public ResultType? Type { get; set; }
+    }
+}
---
+++ mediaTime/ResultType.cs
@@ -0,0 +1,9 @@
+﻿namespace mediaTime
+{
+    public enum ResultType
+    {
+        Success,
+        FailedToRename,
+        Unchanged // Because the timestamp was already proper.
+    }
+}
