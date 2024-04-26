bb132d38879d72a1c000805dbcd55f5a2daa8919
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-04-26 17:13:24 +0900

    Serialization of metadata

--- yyMediaTime/MediaFileInfo.cs
+++ yyMediaTime/MediaFileInfo.cs
@@ -22,25 +22,9 @@ namespace yyMediaTime
         [JsonPropertyName ("extension")]
         public string? Extension { get; set; }
 
-        [JsonIgnore]
-        public IReadOnlyList <MetadataExtractor.Directory>? Metadata { get; set; }
-
-        // Directory.Name, Directory.Tags and Tag.Name are not nullable.
-        // Tag.Description is nullable.
-        // Is Tag.HasName is false, there shouldnt be much reason to include the tag in the JSON.
-        // https://github.com/drewnoakes/metadata-extractor-dotnet/blob/master/MetadataExtractor/Directory.cs
-        // https://github.com/drewnoakes/metadata-extractor-dotnet/blob/master/MetadataExtractor/Tag.cs
-
-        public record DirectoryRecord (string directory_name, IEnumerable <TagRecord> tags);
-
-        public record TagRecord (string name, string? description);
-
-        /// <summary>
-        /// Dont use me.
-        /// </summary>
         [JsonPropertyName ("metadata")]
-        public IEnumerable <DirectoryRecord>? MetadataForSerialization =>
-            Metadata?.Select (x => new DirectoryRecord (x.Name, x.Tags.Where (y => y.HasName).Select (y => new TagRecord (y.Name, y.Description))));
+        [JsonConverter (typeof (MetadataJsonConverter))]
+        public IReadOnlyList <MetadataExtractor.Directory>? Metadata { get; set; }
 
         [JsonPropertyName ("type")]
         [JsonConverter (typeof (JsonStringEnumConverter))]
---
+++ yyMediaTime/MetadataJsonConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using yyLib;
+
+namespace yyMediaTime
+{
+    public class MetadataJsonConverter: JsonConverter <IReadOnlyList <MetadataExtractor.Directory>>
+    {
+        // Directory.Name, Directory.Tags and Tag.Name are not nullable.
+        // Tag.Description is nullable.
+        // If Tag.HasName is false, there shouldnt be much reason to include the tag in the JSON.
+        // https://github.com/drewnoakes/metadata-extractor-dotnet/blob/master/MetadataExtractor/Directory.cs
+        // https://github.com/drewnoakes/metadata-extractor-dotnet/blob/master/MetadataExtractor/Tag.cs
+
+        public override void Write (
+            Utf8JsonWriter writer, IReadOnlyList <MetadataExtractor.Directory> metadata, JsonSerializerOptions options) // "options" is not used.
+        {
+            foreach (var directory in metadata)
+            {
+                writer.WriteStartObject ();
+                writer.WriteString ("directory_name", directory.Name);
+                writer.WriteStartArray ("tags");
+
+                foreach (var tag in directory.Tags.Where (x => x.HasName))
+                {
+                    writer.WriteStartObject ();
+                    writer.WriteString ("name", tag.Name);
+                    writer.WriteString ("description", tag.Description);
+                    writer.WriteEndObject ();
+                }
+
+                writer.WriteEndArray ();
+                writer.WriteEndObject ();
+            }
+        }
+
+        public override IReadOnlyList <MetadataExtractor.Directory> Read (
+            ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+                throw new yyNotSupportedException ("Deserialization not supported.");
+    }
+}
